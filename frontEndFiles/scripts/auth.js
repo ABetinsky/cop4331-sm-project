c02e29c1185378a92eef1e960209fb76302833bc
DANNYSTARLORD <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-02-05 11:21:16 -0500

    Adding the brain of auth page

--- frontEndFiles/scripts/auth.js
+++ frontEndFiles/scripts/auth.js
@@ -1,11 +1,20 @@
-const wrapper = document.querySelector('.wrapper');
+const authModal = document.querySelector('.auth-modal');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
+const alertBox = document.querySelector('.alert-box');
+
 registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
+  authModal.classList.add('active');
 });
 
 loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
+  authModal.classList.remove('active');
 });
+
+setTimeout(() => alertBox.classList.add('show'), 50);
+
+setTimeout(() => {
+  alertBox.classList.remove('show');
+  setTimeout(()=> alertBox.remove(), 500)
+}, 3000);
